d705a9831f1e47ea740ca949be167efbcaa8af16
Pushti Gandhi <shreejipdg@gmail.com>
2026-06-22 14:28:08 +0530

    init

---
+++ src/Prac1_1.kt
@@ -0,0 +1,22 @@
+fun main() {
+    var i=1
+    var j:Int=2
+    var k:Float=3.03f
+    var p:Char='S'
+    var q:String="Pushti"
+    var r:Boolean=true
+    var s:Double=3.25
+    var t:Long=56935739
+    var u:Short=-2
+    var v:Byte=127
+    println("v="+v)
+    println("u="+u)
+    println("t="+t)
+    println("s="+s)
+    println("r="+r)
+    println("q="+q)
+    println("p="+p)
+    println("k="+k)
+    println("j="+j)
+    println("i="+i)
+}
