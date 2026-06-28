89e1a3961df73212cf6c79fd5bf07bd5483e1ab1
Pushti Gandhi <shreejipdg@gmail.com>
2026-06-28 21:51:44 +0530

    init

---
+++ src/Prac1_2.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var x:Int=10
+    var y:Double=x.toDouble()
+    var p:String="10"
+    var q:Int=p.toInt()
+    var r:Double=q.toDouble()
+    println("Integer to Double:")
+    println(x)
+    println(y)
+    println("String to Integer:")
+    println(q)
+    println(p)
+    println("String to Double:")
+    println(q)
+    println(r)
+}
