3ba7bc2fd7980d2a3ab25884ec7d17d030de8f00
Pushti Gandhi <shreejipdg@gmail.com>
2026-06-29 13:40:32 +0530

    init

---
+++ src/Pract1_3.kt
@@ -0,0 +1,35 @@
+import org.jetbrains.annotations.Nullable
+
+fun main(){
+    println("Student Enrollment No.:")
+    val no: Long= readln().toLong()
+    println("Student Name:")
+    val name: String=readln().toString()
+    println("Age:")
+    val age: Int= readln().toInt()
+    println("Student Branch:")
+    val branch: String=readLine().toString()
+    println("Student Class:")
+    val classname: String=readLine().toString()
+    println("Student Batch:")
+    val batch: String=readLine().toString()
+    println("Collage Name:")
+    val collage: String=readLine().toString()
+    println("University Name:")
+    val university: String=readLine().toString()
+    println("Student's Data:")
+    println("Enrollment No.:"+no)
+    println("Name:"+name)
+    println("Age:"+age)
+    println("Branch:"+branch)
+    println("Class:"+classname)
+    println("Batch:"+batch)
+    println("Collage Name:"+collage)
+    println("University Name:"+university)
+
+
+    var s_name: String? = "23"
+    //s_name = "23"
+    val kal :Int= s_name!!.toInt()
+    println(kal)
+}
